1b4772a1ebf2fea24df79b57cde2bb1c9357caf6
Abran Lezama <lezama.abran@gmail.com>
2022-10-20 23:29:02 -0700

    Starting react js, spring boot chat room application

---
+++ src/main/java/com/example/fullstack/chatroom/FullstackChatroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstack.chatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackChatroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackChatroomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstack/chatroom/FullstackChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstack.chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackChatroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
