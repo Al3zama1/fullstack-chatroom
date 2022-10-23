53ad91d466db58a34c4d5b2bda416ce29ea1cb31
Abran Lezama <lezama.abran@gmail.com>
2022-10-23 01:17:26 -0700

    installed antdesign and constructed the base layout of the application

---
+++ src/frontend/src/compoents/ChatRoom.js
@@ -0,0 +1,85 @@
+import "./chatroom.css"
+import 'antd/dist/antd.css';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+const items = [
+    getItem('Chatroom', '1', <UserOutlined />),
+    getItem('Specific User', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
+const Chatroom = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}
+        >
+            <Sider theme="light" collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                {/* <div className="logo" /> */}
+                <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    {/* <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}
+                    >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                        }}
+                    >
+                        Bill is a cat.
+                    </div> */}
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+export default Chatroom;
